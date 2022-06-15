581f8dc096bdaf1281f8bf5e22f14b7884045969
Mario Hernández <marioanhelo@hotmail.com>
2022-06-15 18:58:32 -0500

    Pokedex

--- main.js
+++ main.js
@@ -38,14 +38,15 @@ function getPokemonData(ID){
     function createCardPokemon(pokemon){
         let pokemonName = capitalize(pokemon.data.name)
         let dataCard = `<div class="col-2 mb-4">
-        <div class="card shadow">
+                        <div class="card shadow">
+                        <a href="#" class="card-href" data-bs-toggle="modal" data-bs-target="#pokemonModal" onclick="getPokemonInfo(${pokemon.data.id})">
                         <img src="./img/bck.png" class="card-header border-0">
                         <div class="card-body">
                             <img src="${pokemon.data.sprites.front_default}" alt="Pokemon" class="card-body-img mx-auto d-block">
                             <h4 class="card-body-title text-center">${pokemonName}</h4>
                             <h5 class="text-primary text-center">#${pokemon.data.id.toString().padStart(3,0)}</h5>
                             <h6 class="text-primary text-center">Tipo: ${pokemon.data.types[0].type.name}</h6>
-                          </div>
+                          </div></a>
                     </div></div>`
         container.innerHTML += dataCard
     }
